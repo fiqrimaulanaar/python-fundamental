c1cbb8c92542eb36a799fd49a2156f916f5498f0
Zephilium <wuuuuush@gmail.com>
2020-06-26 10:03:15 +0700

    Initial Commit

--- fundamental1.py
+++ fundamental1.py
@@ -6,6 +6,13 @@
 #
 # print (r.text)
 
+pengenCepat = False
+
+if pengenCepat:
+    print("Jalan Lurus !!")
+else:
+    print("Jalan Muter !!")
+
 jumlahData = 100
 
 for x in range (1, jumlahData+1):
